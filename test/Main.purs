2ace47cefaf1d95436679344f435a1bf6e2243ac
neppord <neppord@gmail.com>
2021-10-16 14:54:24 +0200

    Made the test code a bit more interesting

--- test/Main.purs
+++ test/Main.purs
@@ -1,15 +1,23 @@
 module Test.Main where
 
 import Prelude
-import TreeSitter
 
 import Effect (Effect)
 import Effect.Class.Console (logShow)
+import TreeSitter (children, mkParser, namedChildren, parseString, rootNode)
 
+bashSource :: String
+bashSource = """
+do_stuff () {
+    cat file | grep "foo"
+}
+
+do_stuff
+"""
 main :: Effect Unit
 main = do
   let bashParser = mkParser "bash"
-  let tree = parseString bashParser "cat file | grep 'foo'"
+  let tree = parseString bashParser bashSource
   logShow tree
   tree
     # rootNode

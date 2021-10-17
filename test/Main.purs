78e2b2682b99f7cce793579e625d06ba91036535
neppord <neppord@gmail.com>
2021-10-17 17:52:52 +0200

    Fix imports

--- test/Main.purs
+++ test/Main.purs
@@ -4,7 +4,6 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Class.Console (logShow)
-import TreeSitter.Lazy (children, mkParser, namedChildren, parseString, rootNode)
 import TreeSitter.Declarative (parse)
 
 bashSource :: String

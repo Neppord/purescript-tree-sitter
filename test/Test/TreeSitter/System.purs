14834c697774b0e5f4bdec65a29141319161f1ae
neppord <neppord@gmail.com>
2022-02-12 16:58:06 +0100

    Add testcase for rename

--- test/Test/TreeSitter/System.purs
+++ test/Test/TreeSitter/System.purs
@@ -14,6 +14,11 @@ import Control.Comonad.Cofree (head)
 import Data.Lens.Plated (universe)
 import Data.Array (foldMap)
 import Control.Comonad.Cofree (tail)
+import TreeSitter.Lazy (startIndex)
+import TreeSitter.Lazy (endIndex)
+import Data.Array (foldl)
+import Data.String as String
+import Data.Array (foldr)
 
 program :: String
 program = """
@@ -21,6 +26,7 @@ function_name () {
     cat "my_file"
 }
 other_function_name () {
+    function_name
 }
 """
 tree :: Cofree Array SyntaxNode
@@ -33,6 +39,10 @@ headType :: forall a. Cofree a SyntaxNode -> String
 headType = type' <<< head
 headText :: forall a. Cofree a SyntaxNode -> String
 headText = text <<< head
+
+index :: SyntaxNode -> Tuple Int Int
+index n = Tuple (startIndex n) (endIndex n)
+
 is :: forall a. Eq a => a -> a -> Boolean
 is a = (_ == a)
 headIs :: forall a. String -> Cofree a SyntaxNode -> Boolean
@@ -49,6 +59,7 @@ spec = describe "System" do
             ["function_name"
             , "cat"
             , "other_function_name"
+            , "function_name"
             ]
 
     it "extracts function names from file" do
@@ -63,3 +74,21 @@ spec = describe "System" do
             ["function_name"
             , "other_function_name"
             ]
+    it "rename function names in file" do
+        let ranges = tree
+                # fromFoldable
+                # filter (type' >>> is "word")
+                # filter (text >>> is "function_name")
+                # map index
+            replace string (Tuple start end) p =
+                String.take start p <> string <> String.drop end p
+            program' = foldr (replace "new_function_name") program ranges
+        program' `shouldEqual`
+            """
+new_function_name () {
+    cat "my_file"
+}
+other_function_name () {
+    new_function_name
+}
+"""

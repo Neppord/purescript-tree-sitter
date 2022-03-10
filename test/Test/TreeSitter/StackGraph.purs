b4eb86195d5754b18d690fa5dfd48a455441ff45
neppord <neppord@gmail.com>
2022-03-10 17:35:24 +0100

    Use usage_ instead of void and usage

--- test/Test/TreeSitter/StackGraph.purs
+++ test/Test/TreeSitter/StackGraph.purs
@@ -8,13 +8,9 @@ import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import TreeSitter.StackGraph (CreateGraph, Graph, Node(..), createGraph_, declare, findDefinition, scope, usage)
 import Data.Maybe (Maybe(..))
+import TreeSitter.StackGraph (usage_)
 
--- | this example is for the code below
--- | ```swift
--- | let h = "hello"
--- | let w = "world"
--- | print(h + " " + w)
--- | ```
+
 example :: Graph Int
 example = empty
     # insert 0 (Info { start: 4, end: 5 })
@@ -26,14 +22,20 @@ example = empty
     # insert 6 (Push "h" 5)
     # insert 7 (Push "w" 5)
 
+-- | this example is for the code below
+-- | ```swift
+-- | let h = "hello"
+-- | let w = "world"
+-- | print(h + " " + w)
+-- | ```
 monadExample :: CreateGraph Unit
 monadExample = do
     id1 <- declare "h" { start: 4, end: 5 }
     id2 <- scope [ id1 ]
     id4 <- declare "w" { start: 20, end: 21 }
     id5 <- scope [ id4, id2 ]
-    void $ usage "h" { start: 38, end: 39 } id5
-    void $ usage "w" { start: 48, end: 49 } id5
+    usage_ "h" { start: 38, end: 39 } id5
+    usage_ "w" { start: 48, end: 49 } id5
 
 spec :: Spec Unit
 spec = describe "StackGraph" do

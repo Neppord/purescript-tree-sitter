a996b204f8e4471368b14a422d05429a1bd2f70f
Samuel Ytterbrink <neppord@gmail.com>
2022-04-22 13:56:52 +0200

    Clean test workspace
    Co-authored-by: Olof Bjarnason <olof.bjarnason@proagile.se>

--- test/Test/TreeSitter/System.purs
+++ test/Test/TreeSitter/System.purs
@@ -18,17 +18,6 @@ import TreeSitter.Lazy (SyntaxNode, children, endIndex, mkParser, parseString, r
 import TreeSitter.StackGraph (createGraph_, findDefinition)
 import TreeSitter.StackGraph.Swift (sourceFile)
 
-program :: String
-program =
-    """\
-function_name () {
-    cat "my_file"
-}
-other_function_name () {
-    function_name
-}
-"""
-
 swiftProgram :: String
 swiftProgram =
     """\
@@ -49,74 +38,11 @@ swiftTree = swiftProgram
     # rootNode
     # buildCofree (\s -> Tuple s $ children s)
 
-tree :: Cofree Array SyntaxNode
-tree = program
-    # parseString (mkParser "bash")
-    # rootNode
-    # buildCofree (\s -> Tuple s $ children s)
-
-headType :: forall a. Cofree a SyntaxNode -> String
-headType = type' <<< head
-
-headText :: forall a. Cofree a SyntaxNode -> String
-headText = text <<< head
-
-index :: SyntaxNode -> Tuple Int Int
-index n = Tuple (startIndex n) (endIndex n)
-
 is :: forall a. Eq a => a -> a -> Boolean
 is a = (_ == a)
 
-headIs :: forall a. String -> Cofree a SyntaxNode -> Boolean
-headIs a = is a <<< headType
-
 spec :: Spec Unit
 spec = describe "System" do
-    it "extracts identifiers form a file" do
-        let
-            words = tree
-                # fromFoldable
-                # filter (is "word" <<< type')
-                # map text
-        words `shouldEqual`
-            [ "function_name"
-            , "cat"
-            , "other_function_name"
-            , "function_name"
-            ]
-
-    it "extracts function names from file" do
-        let
-            function_names = tree
-                # universe
-                # fromFoldable
-                # filter (headIs "function_definition")
-                # foldMap tail
-                # filter (headIs "word")
-                # map headText
-        function_names `shouldEqual`
-            [ "function_name"
-            , "other_function_name"
-            ]
-    it "rename function names in file" do
-        let
-            ranges = tree
-                # fromFoldable
-                # filter (type' >>> is "word")
-                # filter (text >>> is "function_name")
-                # map index
-            replace string (Tuple start end) p =
-                String.take start p <> string <> String.drop end p
-            program' = foldr (replace "new_function_name") program ranges
-        program' `shouldEqual`
-            """\
-new_function_name () {
-    cat "my_file"
-}
-other_function_name () {
-    new_function_name
-}
-"""
     it "can create stack graphs" do
         let
             (Tuple index graph) = createGraph_ $ sourceFile swiftTree
